61a92acad2e8686200f0e74cb84b3f334307dfdd
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-10-19 21:33:00 +0545

    Add short docs of model

--- db/model.cjs
+++ db/model.cjs
@@ -1,7 +1,8 @@
 const TABLES = {
-  genre: "genre",
-  animeSeries: "anime_series",
-  animeGenre: "anime_genre",
+  genre: "genre", //consist all genre ( pre defined )
+  animeSeries: "anime_series", // contains anime series that user wishlist, and for more in future
+  animeGenre: "anime_genre", // many to many relationship between anime_series and anime genre
+  animeCollection: "anime_collections", //contains collection info and foreign key and cascade on delete and update
 };
 const DTYPES = { animeStatus: "anime_status" };
 
