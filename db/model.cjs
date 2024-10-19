1a2c9531b43f52aed263cddca03ac9682362aeab
FluffyRudy <81473138+FluffyRudy@users.noreply.github.com>
2024-10-19 21:28:09 +0545

    Delete db/model.cjs

--- db/model.cjs
+++
@@ -1,8 +0,0 @@
-const TABLES = {
-  genre: "genre",
-  animeSeries: "anime_series",
-  animeGenre: "anime_genre",
-};
-const DTYPES = { animeStatus: "anime_status" };
-
-module.exports = { tables: TABLES, dtypes: DTYPES };
