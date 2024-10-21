c9abb8fa42eaab9548ed8541df5dd6b93b21e1b8
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-10-22 00:20:08 +0545

    Remove db check on prod

--- index.cjs
+++ index.cjs
@@ -31,43 +31,6 @@ app.use(
 app.get("/favicon.ico", (req, res) => res.status(204).end());
 app.use("/", inventoryRouter);
 
-function gracefulShutdown() {
-  console.log("Shutting down server...");
-  dbClient.endConnection().then(() => {
-    console.log("Closed DB connections.");
-    process.exit(0);
-  });
-}
-
-async function waitForDatabaseAndStartServer() {
-  const startTime = Date.now();
-  const DB_TIMEOUT_SECONDS = 10;
-
-  while (!dbClient.canQuery) {
-    console.log("Waiting for DB to be ready...");
-    console.log(dbClient.errorLogs);
-    const currentTime = Date.now();
-    if ((currentTime - startTime) / 1000.0 > DB_TIMEOUT_SECONDS) {
-      console.log("Failed to connect database so quitting");
-      await dbClient.endConnection();
-      return;
-    }
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-  }
-
-  app.listen(PORT, () => {
-    console.debug(`Listening at: http://localhost:${PORT}`);
-  });
-}
-
-process.on("SIGTERM", () => {
-  console.log("Receive Termination signal. Cleaning up");
-  gracefulShutdown();
+app.listen(PORT, () => {
+  console.debug(`Listening at: http://localhost:${PORT}`);
 });
-
-process.on("SIGINT", () => {
-  console.log("Receive intruption signal. Cleaning up");
-  gracefulShutdown();
-});
-
-waitForDatabaseAndStartServer();
